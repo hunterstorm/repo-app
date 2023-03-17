d2f883379e1ad93eb9b84d8ce6df67747360ef1d
Hunter Storm <huntersreese@gmail.com>
2023-03-17 01:26:19 -0600

    repo-app pilot commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import UserRepositories from "./components/UserRepositories";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css' 
+import { gsap } from "gsap";
+import HeaderAnimation from "./components/HeaderAnimation";
 
 function App() {
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body>
+      <header>
+      <HeaderAnimation/>
       </header>
-    </div>
-  );
+
+      <UserRepositories username="hunterstorm" />
+    </body>
+  )
 }
 
 export default App;
---
+++ src/components/RepositoryDetails.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const RepositoryDetails = ({ repository }) => {
+  return (
+    <div>
+      
+      <p>{repository.description}</p>
+      <a href={repository.html_url}>Link to Repository</a>
+    </div>
+  );
+};
+
+export default RepositoryDetails;
