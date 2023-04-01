2a1f31aef8450e76e89539af72cba7a20e60dfcb
marvincampbell21 <marvincampbell21@yahoo.com>
2023-04-01 11:55:26 -0400

    CSS update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,10 +3,12 @@ import react from 'react'
 function Home() {
 
     return(
-        <div> 
-        <h2 className = "greeting">Welome to repoLab</h2>
-        </div>
+        <div className= "parentDiv">
+          <div className= "container">
+            <h2 className = "greeting">Welome to repoLab</h2>
+          </div>
+            <p className= "text"> Press the App tab to enter</p>
         
-    )}
-
+        </div>
+      )}
 export default Home
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,37 +4,21 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container'
 
 function Navigation() {
-    return(
-<Navbar className="navbar" expand="lg">
-      <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <ul className="navbar-nav me-auto">
-            <li className="nav-item">
-              <Link to="/home" className="nav-link">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/app" className="nav-link">App</Link>
-            </li>
-          </ul>
-        </Navbar.Collapse>
-      </Container>
+  return (
+    <Navbar className="navbar" expand="lg" style={{ justifyContent: 'flex-end' }}>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <ul className="navbar-nav me-auto">
+          <li className="nav-item">
+            <Link to="/home" className="nav-link">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/app" className="nav-link">App</Link>
+          </li>
+        </ul>
+      </Navbar.Collapse>
     </Navbar>
-    )}
+  );
+}
 
-    export default Navigation
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default  Navigation
